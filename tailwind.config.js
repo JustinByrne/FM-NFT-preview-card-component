9289e4c1bf2c62b516e0d9fee145599f05ac8865
Justin Byrne <theskaterjesus@gmail.com>
2022-02-05 15:33:47 +0000

    Added styling guide colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     fontFamily: {
       'sans': ['Outfit', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          'main-bg': 'hsl(217, 54%, 11%)',
+          'card-bg': 'hsl(216, 50%, 16%)',
+          line: 'hsl(215, 32%, 27%)',
+          soft: 'hsl(215, 51%, 70%)',
+        },
+        cyan: {
+          fm: 'hsl(178, 100%, 50%)',
+        },
+      },
+    },
   },
   plugins: [],
 }
